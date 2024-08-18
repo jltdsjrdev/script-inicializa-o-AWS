6528b0da295d7d3f9e80938d5364800973713fb0
Jorge Junior DEV <113628571+jltdsjrdev@users.noreply.github.com>
2024-08-18 16:45:04 -0300

    update html

--- script-nginx.sh
+++ script-nginx.sh
@@ -6,16 +6,29 @@ apt update -y
 amazon-linux-extras install nginx1.12 -y
 
 # Criar uma página HTML simples
-echo "<html>
-<head>
+echo "<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Servidor Nginx</title>
-</head>
-<body>
-    <h1>Bem-vindo ao servidor Nginx criado por Jorge Junior!</h1>
-    <p>Este servidor está sendo executado em uma instância Amazon Linux configurada automaticamente.
-    Seja Bem-vindo(a)</p>
-</body>
-</html>" > /usr/share/nginx/html/index.html
+    <link rel="stylesheet" href="./style.css" />
+  </head>
+  <body>
+    <h1>Bem-vindo ao servidor Nginx!</h1>
+    <p>
+      Este servidor está sendo executado em uma instância Amazon Linux
+      configurada automaticamente. Seja Bem-vindo(a).
+    </p>
+    <br />
+
+    <h1>Aluno: Jorge LuizTeixeira Da Silva Junior</h1>
+    <br />
+    <p>Id: 1182024</p>
+    <p>Curso DevOps - A.d.a tech - Santander</p>
+  </body>
+</html>
+" > /usr/share/nginx/html/index.html
 
 # Iniciar o serviço Nginx
 systemctl start nginx
